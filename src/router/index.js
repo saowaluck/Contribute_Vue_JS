a5fa9bda6f38dab74df0c17af499856279262369
saowaluck <saowaluck@prontomarketing.com>
2018-05-08 15:51:57 +0700

    add route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+// import Posts from '@/components/Posts'
+// import Comments from '@/components/Comments'
+// import Albums from '@/components/Albums'
+// import Photos from '@/components/Photos'
+// import Todos from '@/components/Todos'
+// import Users from '@/components/Users'
 
 Vue.use(Router)
 
@@ -10,6 +16,36 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    // {
+    //   path: '/posts',
+    //   name: 'Posts',
+    //   component: Posts
+    // },
+    // {
+    //   path: '/comments',
+    //   name: 'Comments',
+    //   component: Comments
+    // },
+    // {
+    //   path: '/albums',
+    //   name: 'Albums',
+    //   component: Albums
+    // },
+    // {
+    //   path: '/photos',
+    //   name: 'Photos',
+    //   component: Photos
+    // },
+    // {
+    //   path: '/todos',
+    //   name: 'Todos',
+    //   component: Todos
+    // },
+    // {
+    //   path: '/users',
+    //   name: 'Users',
+    //   component: Users
+    // }
   ]
 })
